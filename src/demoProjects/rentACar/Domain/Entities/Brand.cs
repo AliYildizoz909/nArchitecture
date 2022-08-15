798ffd536b0394e4a1bec303f19000f492a3af70
Ali Yıldızöz <ali.yildizoz@algomedi.com>
2022-08-16 00:16:09 +0300

    Initial Commit

---
+++ src/demoProjects/rentACar/Domain/Entities/Brand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Persistence.Repositories;
+
+namespace Domain.Entities
+{
+    public class Brand:Entity
+    {
+        public Brand(string name)
+        {
+            Name = name;
+        }
+
+        public Brand(int id, string name) : base(id)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+    }
+}
